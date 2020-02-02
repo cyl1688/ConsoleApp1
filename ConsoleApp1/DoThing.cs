8ce0e3e4175e553180817cd3e760e9a4689c872c
Phil_Chung <phil0418@hotmail.com>
2020-02-02 23:29:24 +0800

    Test

---
+++ ConsoleApp1/DoThing.cs
@@ -0,0 +1,58 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace ConsoleApp1
+{
+    class DoThing
+    {
+        private readonly Timer _timer; //using System.Timers;
+        private static ILogger s_logger; //using NLog;
+
+        static DoThing()
+        {
+            if (s_logger == null)
+            {
+                s_logger = LogManager.GetCurrentClassLogger();
+            }
+        }
+        public DoThing()
+        {
+            this._timer = new Timer(1000) { AutoReset = true };
+            //this._timer.Elapsed += (sender, eventArgs) => Console.WriteLine($"Now Time:{DateTime.Now}");
+            this._timer.Elapsed += (sender, eventArgs) => s_logger.Debug($"Now Time:{DateTime.Now}");
+        }
+
+        public void TTT()
+        {
+
+        }
+
+        public void Start()
+        {
+            this._timer.Start();
+            try
+            {
+                s_logger.Trace($"Timer Start");
+                s_logger.Debug($"Timer Start");
+            }
+            catch(Exception ex)
+            {
+                string strMsg = ex.Message;
+            }
+
+
+        }
+
+        public void Stop()
+        {
+            this._timer.Stop();
+            s_logger.Trace($"Timer Stop");
+            LogManager.Flush();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,130 @@
+﻿using NLog;
+using System;
+using System.Activities.Presentation;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        #region Test
+        //private static ILogger logger;
+        //static void Main(string[] args)
+        //{
+        //    LogManager.ThrowExceptions = true;
+        //    logger = LogManager.GetCurrentClassLogger();
+        //    int logcount = 1000000;
+        //    for (int i = 0; i < 5; i++)
+        //    {
+        //        LogToFile(i, logcount);
+        //    }
+        //}
+
+        //private static void LogToFile(int index, int logcount)
+        //{
+        //    Stopwatch sw = new Stopwatch();
+        //    sw.Start();
+        //    for (int i = 0; i < logcount; i++)
+        //    {
+        //        logger.Debug($"{index}_{i}_{DateTime.Now}");
+        //    }
+        //    sw.Stop();
+        //    logger.Info($"{index}_log 筆數：{logcount};總耗時毫秒：{sw.ElapsedMilliseconds}");
+        //    LogManager.Flush();
+        //}
+        #endregion
+
+        static void Main(string[] args)
+        {
+
+            HostFactory.Run(x =>
+            {
+                x.Service<DoThing>(s =>
+                {
+                    s.ConstructUsing(name => new DoThing());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+                x.UseNLog(); //NLog
+                x.RunAsLocalSystem();
+                var assemblyName = Assembly.GetEntryAssembly().GetName().Name;
+                x.SetDescription("Sample Topshelf Host");
+                x.SetDisplayName(assemblyName);
+                x.SetServiceName(assemblyName);
+            });
+        }
+
+
+
+        //internal class WindowsServiceConfig
+        //{
+        //    public static void Configure()
+        //    {
+        //        HostFactory.Run(x =>
+        //        {
+        //            x.Service<DoThing>(s =>
+        //            {
+        //                s.ConstructUsing(name => new DoThing());
+        //                s.WhenStarted(tc => tc.Start());
+        //                s.WhenStopped(tc => tc.Stop());
+        //            });
+        //            x.RunAsLocalSystem();
+        //            var assemblyName = Assembly.GetEntryAssembly().GetName().Name;
+        //            x.SetDescription("Sample Topshelf Host");
+        //            x.SetDisplayName(assemblyName);
+        //            x.SetServiceName(assemblyName);
+        //        });
+        //    }
+
+        //    public static void ConfigureWithMultiService()
+        //    {
+        //        //using System.Activities.Presentation;
+        //        HostFactory.Run(x =>
+        //        {
+        //            x.Service<ServiceManager>(s =>
+        //            {
+        //                ServiceManager.Container.Add<Service1>();
+        //                ServiceManager.Container.Add<Service2>();
+
+        //                s.ConstructUsing(name => new ServiceManager());
+        //                s.WhenStarted(tc => tc.Start());
+        //                s.WhenStopped(tc => tc.Stop());
+        //            });
+        //            x.UseNLog();
+        //            x.RunAsLocalSystem();
+        //            var assemblyName = Assembly.GetEntryAssembly().GetName().Name;
+        //            x.SetDescription("Sample Topshelf Host");
+        //            x.SetDisplayName(assemblyName);
+        //            x.SetServiceName(assemblyName);
+        //        });
+        //    }
+
+        //    public static void ConfigureWithNLog()
+        //    {
+        //        //using Topshelf;
+        //        HostFactory.Run(x =>
+        //        {
+        //            x.Service<DoThing>(s =>
+        //            {
+        //                s.ConstructUsing(name => new DoThing());
+        //                s.WhenStarted(tc => tc.Start());
+        //                s.WhenStopped(tc => tc.Stop());
+        //            });
+        //            x.UseNLog();
+        //            x.RunAsLocalSystem();
+        //            var assemblyName = Assembly.GetEntryAssembly().GetName().Name; //using System.Reflection;
+        //            x.SetDescription("Sample Topshelf Host");
+        //            x.SetDisplayName(assemblyName);
+        //            x.SetServiceName(assemblyName);
+        //        });
+        //    }
+        //}
+    }
+}
